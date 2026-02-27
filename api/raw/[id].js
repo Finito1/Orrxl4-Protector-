ddca1702755a6b05b7f376fd3aa2e29c800d8b4a
Finito1 <orlandoorrala9@gmail.com>
2026-02-27 13:47:41 -0500

    Update [id].js

--- api/raw/[id].js
+++ api/raw/[id].js
@@ -1,3 +1,12 @@
+import { storage } from "../storage";
+
 export default function handler(req, res) {
-  res.status(200).send("RAW FUNCIONANDO");
+  const { id } = req.query;
+
+  if (!storage[id]) {
+    return res.status(404).send("Not Found");
+  }
+
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send(storage[id]);
 }
